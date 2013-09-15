c9cd9edbaf9bb35b65fda6571ee57cf7d4d9ad8b
Andrew2060 <2060andrew@gmail.com>
2013-09-15 02:39:32 -0700

    Armor Mod Aegis: Remove Unnecessary Checks, limit bonus to 13 blocks

--- src/main/java/net/kingdomsofarden/andrew2060/toolhandler/mods/armormods/ModAegis.java
+++ src/main/java/net/kingdomsofarden/andrew2060/toolhandler/mods/armormods/ModAegis.java
@@ -4,7 +4,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import com.herocraftonline.heroes.Heroes;
@@ -43,19 +42,9 @@ public class ModAegis extends ArmorMod {
         } else {
             p.addPotionEffect(PotionEffectType.DAMAGE_RESISTANCE.createEffect(20, 1));
             for(Hero member : hP.getMembers()) {
-                for(PotionEffect pE : member.getEntity().getActivePotionEffects()) {
-                    if(pE.getType().equals(PotionEffectType.DAMAGE_RESISTANCE)) {
-                        if(pE.getAmplifier() > 1) {
-                            return;
-                        }
-                        if(pE.getDuration() > 20) {
-                            return;
-                        }
-                    } else {
-                        continue;
-                    }
+                if(member.getPlayer().getLocation().distanceSquared(p.getLocation()) < 169) {
+                    pEMan.addPotionEffectStacking(PotionEffectType.DAMAGE_RESISTANCE.createEffect(20, 1),member.getEntity(),false);
                 }
-                pEMan.addPotionEffectStacking(PotionEffectType.DAMAGE_RESISTANCE.createEffect(20, 1),member.getEntity(),false);
             }
         }
     }
