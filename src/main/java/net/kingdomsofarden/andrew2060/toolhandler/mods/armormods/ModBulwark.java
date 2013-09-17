aa9b40ce196165e4981dc9dddd10a04d57bd4ff1
Andrew2060 <2060andrew@gmail.com>
2013-09-17 13:05:28 -0700

    Bulwark: 10% M-Resist, +3 Prot, 30% Proj Damage Reduct, 0.25% Chance

---
+++ src/main/java/net/kingdomsofarden/andrew2060/toolhandler/mods/armormods/ModBulwark.java
@@ -0,0 +1,32 @@
+package net.kingdomsofarden.andrew2060.toolhandler.mods.armormods;
+
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+
+import com.herocraftonline.heroes.api.events.WeaponDamageEvent;
+
+import net.kingdomsofarden.andrew2060.toolhandler.mods.typedefs.ArmorMod;
+
+public class ModBulwark extends ArmorMod {
+
+    public ModBulwark() {
+        super("Bulwark", 1, true, "Reduces all projectile damage by 30%");
+        setMagicResist(10.00);
+        setProtBonus(3.00);
+    }
+
+    @Override
+    public void executeOnArmorDamage(WeaponDamageEvent event) {
+        if(event.getAttackerEntity() instanceof Projectile) {
+            event.setDamage(event.getDamage() * 0.7);
+            return;
+        }
+    }
+
+    @Override
+    public void executeOnTick(Player p) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
